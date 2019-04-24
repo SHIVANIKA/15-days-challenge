8d148d904be9a0dbbb1e75738d3d58d6acf77a26
unknown <shivanika12@gmail.com>
2019-04-24 23:04:47 +0530

    variables

---
+++ Variables.java
@@ -0,0 +1,22 @@
+public class FirstClass {
+    public static void main(String [] args){
+        System.out.println("hello world!");
+        int s = (10*9)+(6*45);
+        int h = 45;
+        int  i =67+s;
+        int Total = s+h+i;
+        int LastOne = 2000 - Total;
+        System.out.println(LastOne);
+    }
+}
+
+
+
+
+
+
+OUTPUT:
+hello world!
+1168
+
+Process finished with exit code 0
