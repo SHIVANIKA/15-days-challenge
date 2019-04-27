d6d5cde7d32be7b805c5ac942046ccee7a2e0267
unknown <shivanika12@gmail.com>
2019-04-27 23:17:57 +0530

    IfKeyword

---
+++ IfKeyword.java
@@ -0,0 +1,27 @@
+public class FirstClass {
+    public static void main(String [] args){
+
+            boolean gameover = true;
+            int FirstScore = 5000;
+            int LevelCompleted = 5 ;
+            int Bonus = 100;
+            if (gameover== true){
+                int FirstFinalScore = FirstScore + (LevelCompleted * Bonus);
+                System.out.println("FirstFinalScore ="+ FirstFinalScore);
+
+            }
+            boolean NewGameOver = false;
+            int SecondScore = 10000;
+            int NextLevelCompleted = 8;
+            int NextBonus = 200;
+
+            if(NewGameOver==false){
+                int SecondFinalScore = SecondScore + (NextLevelCompleted * NextBonus);
+                System.out.println("SecondFinalScore = "+ SecondFinalScore);
+            }
+
+OUTPUT
+FirstFinalScore =5500
+SecondFinalScore = 11600
+
+Process finished with exit code 0
