9ec16d91baa0c4f0d745c277d90b9af5ceaa86d7
unknown <shivanika12@gmail.com>
2019-05-01 23:08:08 +0530

    Switch_case

---
+++ Switch.java
@@ -0,0 +1,37 @@
+package com.shivanika;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char newChar = 'A';
+        switch(newChar){
+            case 'A':
+                System.out.println("found A");
+                break;
+            case 'B' :
+                System.out.println("found B");
+                break;
+            case 'C':
+                System.out.println("found C");
+                break;
+            case 'D':
+                System.out.println("found D");
+                break;
+            case 'E':
+                System.out.println("found E");
+                break;
+            default:
+                System.out.println("not found");
+                break;
+
+
+        }
+
+    }
+}
+
+OUTPUT:
+found A
+
+Process finished with exit code 0
+
